5668c08a4b7926ba7b57e4edaaeda4f0a3f4a03e
damooola <damolanelson@gmail.com>
2024-08-16 08:06:27 +0100

    edit main file

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider( create:(context) => Cart(),
-      builder: (contex,child)=>  const MaterialApp(
+      builder: (context,child)=>  const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: IntroPage(),
       ),
